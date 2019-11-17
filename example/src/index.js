dfa139faa3f0b680d6c489847524b24d17a5ca7d
Boy with Silver Wings <agney@outlook.in>
2019-11-18 00:24:48 +0530

    check in example

--- example/src/index.js
+++ example/src/index.js
@@ -9,7 +9,13 @@ const App = () => {
     css: `h1 {color: red}`,
     javascript: `console.log('this')`,
   };
-  return <LiveEditor initialSnippet={snippet} />;
+  return (
+    <LiveEditor
+      initialSnippet={snippet}
+      defaultEditorTab="css"
+      defaultResultTab="console"
+    />
+  );
 };
 
 const rootEl = document.getElementById("root");
