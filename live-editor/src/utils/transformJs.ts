2ad3a39e12c0406704b9a281bf82edc82e08fefc
Boy with Silver Wings <agney@outlook.in>
2019-11-17 14:25:15 +0530

    add docs for transform js

--- live-editor/src/utils/transformJs.ts
+++ live-editor/src/utils/transformJs.ts
@@ -1,6 +1,10 @@
 import * as Babel from "@babel/standalone";
 import BabelPluginUnpkg from "babel-plugin-unpkg";
 
+/**
+ * Babelify the JavaScript code and apply the unpkg plugin to transform modules.
+ * @param code JavaScript code
+ */
 function transformJs(code: string) {
   return Babel.transform(code, {
     plugins: [BabelPluginUnpkg],
