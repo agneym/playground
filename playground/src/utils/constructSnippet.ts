55f83baae4f0f15a64c9797712c273d4cc31d9d5
Boy with Silver Wings <agney@outlook.in>
2019-11-20 12:59:22 +0530

    make babel work from within the snippet

--- playground/src/utils/constructSnippet.ts
+++ playground/src/utils/constructSnippet.ts
@@ -1,5 +1,6 @@
+import BabelPluginUnpkg from "babel-plugin-unpkg";
+
 import { ISnippet } from "../types";
-import transformJs from "./transformJs";
 
 /**
  * Constructs snippet from individual html, css and js code.
@@ -23,15 +24,66 @@ function constructSnippet({ markup, css, javascript }: ISnippet, id: string) {
         console.log = function(...rest) {
           if(typeof window !== 'undefined') {
             window.parent.postMessage({
-              source: "frame-${id}",
+              source: "frame-m",
               message: rest,
             }, "*");
           }
           _privateLog.apply(console, arguments);
         }
       </script>
+      <script src="https://unpkg.com/@babel/standalone/babel.min.js"></script>
       <script type="module">
-        ${transformJs(javascript)}
+        const BareIdentifierFormat = /^((?:@[^\\/]+\\/)?[^\\/]+)(\\/.*)?$/
+
+        function validUrl(url) {
+          try {
+            new URL(url);
+            return true;
+          } catch(err) {
+            return false;
+          }
+        }
+
+        function unpkg(dependencies = {}) {
+          return {
+            visitor: {
+              "ImportDeclaration|ExportNamedDeclaration|ExportAllDeclaration"(path) {
+                if (
+                  !path.node.source || // Probably a variable declaration
+                  validUrl(path.node.source.value) || // Valid URL
+                  path.node.source.value.substr(0, 2) === "//" || // URL w/o protocol
+                  [".", "/"].indexOf(path.node.source.value.charAt(0)) >= 0 // Local path
+                )
+                  return; // Leave it alone
+
+                // A "bare" identifier
+                const match = BareIdentifierFormat.exec(path.node.source.value);
+                const packageName = match[1];
+                const file = match[2] || "";
+
+                console.warn(
+                  dependencies[packageName],
+                  'Missing version for package "%s" in dependencies; falling back to "latest"',
+                  packageName
+                );
+
+                const version = dependencies[packageName] || "latest";
+
+                path.node.source.value = \`https://unpkg.com/\${packageName}@\${version}\${file}?module\`;
+              }
+            }
+          };
+        }
+
+        Babel.registerPlugin('unpkg', unpkg);
+
+        const code = Babel.transform(decodeURI(\`${encodeURI(javascript)}\`), {
+          plugins: ['unpkg'],
+        }).code;
+        const script = document.createElement("script");
+        script.type = "module";
+        script.innerHTML = code;
+        document.body.appendChild(script);
       </script>
     </body>
     </html>
