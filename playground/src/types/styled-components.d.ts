f52b4d744d5b2428a9aee4da1e2a07a6e36b6cdb
Agney <agney@outlook.in>
2019-12-18 21:08:04 +0530

    change from static definition to types

--- playground/src/types/styled-components.d.ts
+++ playground/src/types/styled-components.d.ts
@@ -1,28 +1,8 @@
 import "styled-components";
 
+import theme from "../utils/theme";
+
 declare module "styled-components" {
-  export interface DefaultTheme {
-    container: {
-      border: string;
-      minHeight: string;
-    };
-    error: {
-      background: string;
-      color: string;
-    };
-    console: {
-      background: string;
-    };
-    tabs: {
-      tabHeader: {
-        borderBottom: string;
-      };
-      tabPanel: {
-        phoneHeight: string;
-      };
-      selectedTab: {
-        borderBottom: string;
-      };
-    };
-  }
+  type Theme = typeof theme;
+  export interface DefaultTheme extends Theme {}
 }
