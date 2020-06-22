75e96d180792fbd938aab701fb35f1b7c5d0578d
Agney <agney.menon@bigbinary.com>
2020-06-22 09:45:05 +0530

    types

--- playground/types/styled-components.d.ts
+++ playground/types/styled-components.d.ts
@@ -1,6 +1,6 @@
 import "styled-components";
 
-import { theme } from "../utils/theme";
+import { theme } from "../src/utils/theme";
 
 declare module "styled-components" {
   type Theme = typeof theme;
