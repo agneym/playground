4b8eb89b34ec4b5f5ebc80560aa1d5c57bcbebda
Agney <agney.menon@bigbinary.com>
2021-03-13 16:20:20 +0530

    add goober and set it up with theme

--- playground/types/styled-components.d.ts
+++ playground/types/styled-components.d.ts
@@ -1,8 +1,8 @@
-import "styled-components";
+import "goober";
 
 import { theme } from "../src/utils/theme";
 
-declare module "styled-components" {
-  type Theme = typeof theme;
-  export interface DefaultTheme extends Theme {}
+declare module "goober" {
+  type OurTheme = typeof theme;
+  export interface DefaultTheme extends OurTheme {}
 }
